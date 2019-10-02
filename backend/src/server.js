29576d5c50f3e5578247a31b9719eded3d41d105
Armando Luiz Bretas Neto <armando.neto@gazin.com.br>
2019-10-02 19:49:00 -0300

    front end finalizado

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -15,7 +17,8 @@ mongoose.connect(
 // req.query = Acessar Query Params ( para filtros )
 // req.params = Acessar Route params ( para edição , delete)
 // req.body = acessar corpo da requiisição ( para criação, edição )
-
+app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 app.listen(3333);
