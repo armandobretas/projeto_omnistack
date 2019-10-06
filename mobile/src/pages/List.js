aa94526a2a0a6e83dcc36c3036751e5dd1afe2d0
Armando Luiz Bretas Neto <armando.neto@gazin.com.br>
2019-10-06 00:03:16 -0300

    final commit

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-
+import socketio from "socket.io-client";
 import {
   SafeAreaView,
+  Alert,
   ScrollView,
   StyleSheet,
   AsyncStorage,
@@ -14,6 +15,22 @@ import logo from "../assets/logo.png";
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("user").then(user_id => {
+      const socket = socketio("http://192.168.100.184:3333", {
+        query: { user_id }
+      });
+
+      socket.on("booking_response", booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? "APROVADA" : "REJEITADA"
+          } `
+        );
+      });
+    });
+  }, []);
 
   useEffect(() => {
     AsyncStorage.getItem("techs").then(storagedTechs => {
