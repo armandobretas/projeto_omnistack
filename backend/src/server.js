20fec5d6ffaf01f7e66dcff6fcb6fb37781f58bc
Armando Luiz Bretas Neto <armando.neto@gazin.com.br>
2019-10-05 21:06:00 -0300

    att backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,12 @@ const routes = require("./routes");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+const socketio = require("socket.io");
+const http = require("http");
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@omnistack-o8cij.mongodb.net/semana09?retryWrites=true&w=majority",
@@ -14,11 +18,27 @@ mongoose.connect(
   }
 );
 
-// req.query = Acessar Query Params ( para filtros )
-// req.params = Acessar Route params ( para edição , delete)
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+// req.query  = Acessar Query Params ( para filtros )
+// req.params ( oq vem por get ) = Acessar Route params ( para edição , delete)
 // req.body = acessar corpo da requiisição ( para criação, edição )
+
 app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
-app.listen(3333);
+
+server.listen(3333);
